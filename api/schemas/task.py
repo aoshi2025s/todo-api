093059f4e85c8c6ec0192b2b02b12ca26c589e6b
aoshi0215 <51108018+aoshi0215@users.noreply.github.com>
2023-11-01 06:50:19 +0900

    mysqlの前まで

---
+++ api/schemas/task.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel,Field
+
+#BaseModelはfastapiのスキーマモデルを表す
+
+class TaskBase(BaseModel):
+    title: str or None = Field(None,exmaple="ゴミ捨てに行く")
+
+class Task(TaskBase):
+    id: int
+    #TaskBaseでTaskとTaskCreateの共通項を抽象
+    #title: str or None = Field(None,example="ゴミ捨てに行く")
+    done: bool = Field(False,description="完了フラグ")
+
+    class Config: #DB操作で使用
+        orm_mode = True
+
+class TaskCreate(TaskBase):
+    #TaskBaseに抽象
+    #title: str or None = Field(None,example="ゴミ捨てに行く")
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
