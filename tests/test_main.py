9c89d45240b1b15c18950dac5305ca636567ea1d
aoshi0215 <51108018+aoshi0215@users.noreply.github.com>
2023-11-02 04:07:56 +0900

    due_dateの追加とテスト

--- tests/test_main.py
+++ tests/test_main.py
@@ -39,10 +39,11 @@ async def async_client() -> AsyncClient:
 @pytest.mark.asyncio
 async def test_create_and_read(async_client):
     response = await async_client.post("/tasks",
-    json={"title":"テストタスク"})
+    json={"title":"テストタスク","due_date":"2024-12-01"})
     assert response.status_code == starlette.status.HTTP_200_OK
     response_obj = response.json()
     assert response_obj["title"] == "テストタスク"
+    
 
     response = await async_client.get("/tasks")
     assert response.status_code == starlette.status.HTTP_200_OK
@@ -50,10 +51,11 @@ async def test_create_and_read(async_client):
     assert len(response_obj) == 1
     assert response_obj[0]["title"] == "テストタスク"
     assert response_obj[0]["done"] is False
+    
 
 @pytest.mark.asyncio
 async def test_done_flag(async_client):
-    response = await async_client.post("/tasks",json={"title":"テストタスク2"})
+    response = await async_client.post("/tasks",json={"title":"テストタスク2","due_date":"2024-12-01"})
     assert response.status_code == starlette.status.HTTP_200_OK
     response_obj = response.json()
     assert response_obj["title"] == "テストタスク2"
@@ -73,3 +75,20 @@ async def test_done_flag(async_client):
     #既に完了フラグが外れているので404を返却
     response = await async_client.delete("/tasks/1/done")
     assert response.status_code == starlette.status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "input_param, expectation",
+    [
+        ("2024-12-01",starlette.status.HTTP_200_OK),
+        ("2024-12-32",starlette.status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ("2024/12/01",starlette.status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ("2024-1201",starlette.status.HTTP_422_UNPROCESSABLE_ENTITY),
+    ],
+)
+async def test_due_date(input_param,expectation,async_client):
+    response = await async_client.post(
+        "/tasks", json={"title":"テストタスク","due_date":input_param}
+    )
+    assert response.status_code == expectation
